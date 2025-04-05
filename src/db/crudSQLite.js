813484217af9e44ddb45059a03430f546b51b2e3
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-05 16:04:25 +0530

    CRUD SQLite

--- src/db/crudSQLite.js
+++ src/db/crudSQLite.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import initializeDatabase from "./initializeDB";
 const sqlLiteCRUD=async()=>{
     const [db,setDB]=useState(null);
+    const [posts,setPosts]=useState([]);
 
     useEffect(() => {
         initialize();
@@ -13,10 +14,32 @@ const sqlLiteCRUD=async()=>{
         const database=await initializeDatabase();
         setDB(database);
     }
-    const createTable=()=>{
-
+    const addPost=(post)=>{
+        const { title, body } = post;
+        db.run(`INSERT INTO posts (title, body) VALUES (?, ?)`, [title, body]);
     }
 
+    const deletePost=(id)=>{
+        db.run(`DELETE FROM posts WHERE id = ?`, [id]);
+    }
+
+    const getPosts=()=>{
+        const posts = db.exec("SELECT * FROM posts");
+        setPosts(posts)
+    }
+
+    const saveToIndexedDB = (db) => {
+        const data = db.export();
+        const request = indexedDB.open("myDb", 1);
+      
+        request.onsuccess = (event) => {
+          const dbRequest = event.target.result;
+          const transaction = dbRequest.transaction("sqlite", "readwrite");
+          const store = transaction.objectStore("sqlite");
+          store.put(data, "dbFile");
+        };
+      };
+      
 
     return {
         createTable
