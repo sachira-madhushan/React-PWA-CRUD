c74c7938c33879f0afbf219029a06de45d238858
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-21 16:52:48 +0530

    save user data in indexed db when login with encrypted password

--- src/indexedDB/userData.js
+++ src/indexedDB/userData.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { openDB } from "idb";
+import bcrypt from 'bcryptjs'
+
 const userData = () => {
     const [db, setDb] = useState();
 
@@ -9,8 +11,8 @@ const userData = () => {
             const db = await openDB('userDB', 1, {
                 upgrade(db) {
                     if (!db.objectStoreNames.contains('user')) {
-                        const store = db.createObjectStore('user',{
-                            keyPath: "key" 
+                        const store = db.createObjectStore('user', {
+                            keyPath: "key"
                         });
                     }
                 },
@@ -22,19 +24,26 @@ const userData = () => {
         initializeDB();
     }, []);
 
-    const setUserData = async (expire_date, last_sync, user_name,user_email,user_password) => {
-        const transaction =await db.transaction('user', 'readwrite');
-        const store = await transaction.objectStore('user');
+    const setUserData = async (expire_date, last_sync, user_name, user_email, user_password) => {
+        try {
+            const transaction = await db.transaction('user', 'readwrite');
+            const store = await transaction.objectStore('user');
 
-        store.put({key:"expire_date",value:expire_date});
-        store.put({key:"last_sync",value:last_sync});
-        store.put({key:"user_name",value:user_name});
-        store.put({key:"user_email",value:user_email});
-        store.put({key:"user_password",value:user_password});
+            const salt = bcrypt.genSaltSync(10);
+            const hashed_password = bcrypt.hashSync(user_password, salt);
+
+            store.put({ key: "expire_date", value: expire_date });
+            store.put({ key: "last_sync", value: last_sync });
+            store.put({ key: "user_name", value: user_name });
+            store.put({ key: "user_email", value: user_email });
+            store.put({ key: "user_password", value: hashed_password });
+        } catch (error) {
+            alert(error)
+        }
     }
 
-    const getUserName=async()=>{
-        const transaction =await db.transaction('user', 'readwrite');
+    const getUserName = async () => {
+        const transaction = await db.transaction('user', 'readwrite');
         const store = await transaction.objectStore('user');
         return await store.get("user_name");
     }
